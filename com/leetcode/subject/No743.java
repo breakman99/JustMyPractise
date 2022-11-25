d3b2b1393572190cef119eac524883303b8c93cb
breakman <breakman99@foxmail.com>
2022-11-26 00:03:54 +0800

    Leetcode No743: 1. Dijkstra 邻接表实现 2. Floyd 邻接矩阵实现

--- com/leetcode/subject/No743.java
+++ com/leetcode/subject/No743.java
@@ -1,8 +1,15 @@
 package com.leetcode.subject;
+
 import java.util.*;
+
 public class No743 {
     public static void main(String[] args) {
-        System.out.println();
+        int[][] times = new int[][]{
+                {2, 1, 1},
+                {2, 3, 1},
+                {3, 4, 1}
+        };
+        System.out.println(new No743_Solution02().networkDelayTime(times, 4, 2));
     }
 }
 
@@ -55,23 +62,37 @@ class No743_Solution02 {
     int N = 102;
     int[] he = new int[N];
     int[] ne = new int[M];
-    int[] e  = new int[M];
-    int[] w  = new int[M];
-    int idx = 0;
+    int[] e = new int[M];
+    int[] w = new int[M];
+    int idx = 1;
+
     public int networkDelayTime(int[][] times, int n, int k) {
         // create graph
         for (int[] time : times) {
             add(time[0], time[1], time[2]);
         }
-        int[] path = new int[n];
-        boolean[] used = new boolean[n];
+        int[] path = new int[n + 1];
+        Arrays.fill(path, Integer.MAX_VALUE >> 1);
+        path[k] = 0;
+        boolean[] used = new boolean[n + 1];
         PriorityQueue<Integer[]> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         queue.add(new Integer[]{k, 0});
         while (!queue.isEmpty()) {
             Integer[] temp = queue.remove();
             int t = temp[0];
+            used[t] = true;
+            List<Integer> edges = getEdges(t);
+            for (int i : edges) {
+                path[e[i]] = Math.min(path[e[i]], path[t] + w[i]);
+                if (!used[e[i]]) queue.add(new Integer[]{e[i], w[i]});
+            }
         }
-        return 0;
+        int max = -1;
+        for (int i = 1; i < path.length; i++) {
+            if (path[i] == Integer.MAX_VALUE >> 1) return -1;
+            max = Math.max(max, path[i]);
+        }
+        return max;
     }
 
     // a:起点  b:终点  c:边权值
@@ -82,9 +103,42 @@ class No743_Solution02 {
         he[a] = idx;
         idx++;
     }
+
+    List<Integer> getEdges(int a) {
+        List<Integer> edges = new ArrayList<>();
+        for (int i = he[a]; i != 0; i = ne[i]) {
+            edges.add(i);
+        }
+        return edges;
+    }
 }
 
-
+// floyd 邻接矩阵
+class No743_Solution03 {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[][] graph = new int[n + 1][n + 1];
+        int VAL = Integer.MAX_VALUE >> 1;
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(graph[i], VAL);
+            graph[i][i] = 0;
+        }
+        for (int[] time : times) {
+            graph[time[0]][time[1]] = time[2];
+        }
+        for (int l = 1; l <= n; l++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][l] + graph[l][j]);
+                }
+            }
+        }
+        int max = -1;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, graph[k][i]);
+        }
+        return max >= VAL ? -1 : max;
+    }
+}
 
 
 
