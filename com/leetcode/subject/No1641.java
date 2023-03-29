e386855b72b12bb549e5ce8c4f439a23a2d386bd
breakman99 <breakman99@foxmail.com>
2023-03-30 01:01:17 +0800

    feature: 1. daily problem

---
+++ com/leetcode/subject/No1641.java
@@ -0,0 +1,14 @@
+package com.leetcode.subject;
+
+public class No1641 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class No1641Solution {
+    public int countVowelStrings(int n) {
+
+        return 0;
+    }
+}
---
+++ com/test/Test3.java
@@ -0,0 +1,19 @@
+package com.test;
+
+
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class Conversion {
+    public int AToNum(String str) {
+        return 0;
+    }
+
+    public String NumToA(int num) {
+        return null;
+    }
+}
